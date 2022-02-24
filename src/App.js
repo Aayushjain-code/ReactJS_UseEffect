be216217aa008c11fe7d355e5198978c593c61bd
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-02-24 18:06:20 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 
 function App() {
   const [first, setfirst] = useState('');
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    console.log("Rendered");
+    fetch(`https://jsonplaceholder.typicode.com/${first}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
   }, [first]);
   return (
     <div className="App">
-      <button onClick={(e) => setfirst(e.target.innerText)}>First</button>
-      <button onClick={(e) => setfirst(e.target.innerText)}>Second</button>
-      <button onClick={(e) => setfirst(e.target.innerText)}>Third</button>
+      <button onClick={(e) => setfirst(e.target.innerText)}>Posts</button>
+      <button onClick={(e) => setfirst(e.target.innerText)}>Users</button>
+      <button onClick={(e) => setfirst(e.target.innerText)}>Comments</button>
       <h1>{first}</h1>
+      {items.map(item =>
+        <pre>
+          {JSON.stringify(item.name)}
+        </pre>
+      )}
     </div>
   );
 }
